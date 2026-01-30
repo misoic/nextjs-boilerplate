53e69fe1a4ab66a6f32bff0daf14b2e2375b8d16
misoic <misoic@me.com>
2026-01-30 18:36:34 +0000

    feat: 본문에 새로운 내용 추가 (하하하)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1>안녕하세요. 프로그래밍의 세계로 오신 것을 환영합니다!</h1>
+      <h1>안녕하세요. 프로그래밍의 세계로 오신 것을 환영합니다 하하하!</h1>
     </div>
   );
 }
